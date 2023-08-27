fcf60cbe5b3d32f148406f19ef3606f9cd819fe7
Berkaytoslak <berkaytslk15@gmail.com>
2023-08-27 13:34:41 +0300

    hospital create

---
+++ src/main/java/com/hospitalsystem/hospitalsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,115 @@
+package com.hospitalsystem.hospitalsystem.configuration;
+
+import com.hospitalsystem.hospitalsystem.database.repository.UserEntityRepository;
+import com.hospitalsystem.hospitalsystem.util.security.JWTFilter;
+import com.hospitalsystem.hospitalsystem.util.security.SecurityService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private UserEntityRepository userRepo;
+    @Autowired
+    private JWTFilter filter;
+    @Autowired
+    private SecurityService uds;
+
+
+    //TODO USER ADMİN VE DOCTOR ROLLERİNİ EKLE
+    private static final String[] AUTH_WHITELIST = {
+            "/auth/**",
+            "/swagger-ui/**",
+            "v3/api-docs/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/api/public/**",
+            "/api/public/authenticate",
+            "/actuator/*",
+            "/swagger-ui/**",
+            "/tax",
+            "/tax/**",
+            "/category/**",
+            "/category",
+            "/product/**",
+            "/product"
+
+    };
+
+
+    private static final String[] USER_AUTH_WHITELIST = {
+            "/selam"
+
+    };
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("security");
+        http.headers().frameOptions().disable();
+        http.csrf().disable()
+                .httpBasic().disable()
+                .cors()
+                .configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    configuration.setExposedHeaders(List.of("Content-Disposition"));
+                    return configuration;
+                }).and()
+                .authorizeHttpRequests()
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .requestMatchers(USER_AUTH_WHITELIST).hasRole("user")
+                .requestMatchers(USER_AUTH_WHITELIST).hasRole("admin")
+                .requestMatchers(USER_AUTH_WHITELIST).hasRole("doctor")
+                .and()
+
+                .userDetailsService(uds)
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                )
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+}
